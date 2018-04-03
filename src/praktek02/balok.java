70f12abe170cf19ccd2fccee462b0e3d321b1766
rahmatbayu <32393970+rahmatbayu@users.noreply.github.com>
2018-04-02 23:05:59 -0700

    construktor-sama dengan nama class

--- src/praktek02/balok.java
+++ src/praktek02/balok.java
@@ -5,6 +5,12 @@ public class balok {
     double panjang;
     double lebar;
     double tinggi;
+
+    public balok() {
+        panjang=4;
+        lebar=6;
+        tinggi=8;
+    }
     
     void cetakinfo(){
         System.out.println("================");
@@ -19,5 +25,7 @@ public class balok {
         volume = panjang*lebar*tinggi;
         return volume;
     }
-    
+    void cetakvolume(){
+        System.out.println("volumenya adalah:"+hitungvolume());
+    }
 }
