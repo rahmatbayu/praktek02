103496492b6b93fe3fdd228ecc62c76868aa0ccb
rahmatbayu <32393970+rahmatbayu@users.noreply.github.com>
2018-04-02 22:32:57 -0700

    mengisi nilai atribute

--- src/praktek02/balokaksi.java
+++ src/praktek02/balokaksi.java
@@ -3,6 +3,10 @@ package praktek02;
 
 public class balokaksi {
     public static void main(String[] args) {
+        balok b1 = new balok();
+        b1.panjang = 12;
+        b1.lebar = 8;
+        b1.tinggi = 6;
         
     }
     
