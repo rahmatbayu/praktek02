a63257ced9523015f65635850563dafc77ffab4e
rahmatbayu <32393970+rahmatbayu@users.noreply.github.com>
2018-04-02 22:18:07 -0700

    membuat atribut dan behavior

--- src/praktek02/balok.java
+++ src/praktek02/balok.java
@@ -2,6 +2,17 @@
 package praktek02;
 
 public class balok {
+    double panjang;
+    double lebar;
+    double tinggi;
     
+    void cetakinfo(){
+        System.out.println("================");
+         System.out.println("panjang :"+panjang);
+          System.out.println("lebar :"+lebar);
+           System.out.println("tinggi:"+tinggi);
+            System.out.println("================");
+        
+    }
     
 }
